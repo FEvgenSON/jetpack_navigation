324f118d6b12e059041a8c20a14b4fe66c09610c
fevgenson <fevgenson@gmail.com>
2020-10-28 16:32:24 +0700

    popUp

--- app/src/main/java/com/example/jetpacknaviagtion/BottomNavigationFragment.kt
+++ app/src/main/java/com/example/jetpacknaviagtion/BottomNavigationFragment.kt
@@ -1,17 +1,26 @@
 package com.example.jetpacknaviagtion
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_bottom_navigation.*
 
 class BottomNavigationFragment : Fragment(R.layout.fragment_bottom_navigation) {
 
+    private companion object {
+        var instanceNumber = 0
+    }
+
+    init {
+        instanceNumber++
+    }
+
+    private val localInstanceNumber = instanceNumber
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        text.text = "BottomNavigationFragment, instance number $localInstanceNumber"
         full_screen_fragment_1_navigation_button.setOnClickListener {
             findNavController().navigate(R.id.bottom_navigation_fragment_to_full_screen_fragment_1)
         }
--- app/src/main/java/com/example/jetpacknaviagtion/FullScreen1Fragment.kt
+++ app/src/main/java/com/example/jetpacknaviagtion/FullScreen1Fragment.kt
@@ -3,6 +3,7 @@ package com.example.jetpacknaviagtion
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_full_screen1.*
 
 class FullScreen1Fragment : Fragment(R.layout.fragment_full_screen1) {
@@ -15,8 +16,13 @@ class FullScreen1Fragment : Fragment(R.layout.fragment_full_screen1) {
         instanceNumber++
     }
 
+    private val localInstanceNumber = instanceNumber
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        text.text = "FullScreen1Fragment, instance number $instanceNumber"
+        text.text = "FullScreen1Fragment, instance number $localInstanceNumber"
+        full_screen_fragment_1_navigation_button.setOnClickListener {
+            findNavController().navigate(R.id.full_screen_fragment_1_to_full_screen_fragment_1)
+        }
     }
 }
